776b923adebf9568cccaef73fa72ce4c5961c4bf
horacio23 <horacio.delgado23@gmail.com>
2016-03-31 22:00:41 -0400

    added the basic project structure

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('./public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000, function() {
+  console.log('Listening to Port 3000');
+});
