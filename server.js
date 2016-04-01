63dd5bfdc691866c30f2a8de7f82a77a686d037e
horacio23 <horacio.delgado23@gmail.com>
2016-04-01 10:40:04 -0400

    modified server.js to work with the ejs templating engine. Also added some new dependencies

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(3000, function() {
