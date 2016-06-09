da2b81be57e5b0f4218a0056fab6a744e6b30444
Horacio Delgado <horacio.delgado23@gmail.com>
2016-06-09 14:49:31 -0400

    some more updates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('./public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -9,6 +11,6 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(3000, function() {
-  console.log('Listening to Port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Listening to Port '+app.get('port'));
 });
