2fc6ba5a6101e5e1c37530be4b69791362d72855
horacio23 <horacio.delgado23@gmail.com>
2016-06-14 15:41:00 -0400

    moved the morris code to its own file

---
+++ public/js/customMorris.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    Morris.Donut({
+      element: 'graph',
+      data: [
+        {value: 35, label: 'Java', id:'java'},
+        {value: 15, label: 'Hadoop', id:'hadoop'},
+        {value: 17, label: 'Javascript', id:'js'},
+        {value: 13, label: 'Web Services', id:'webServices'},
+        {value: 5, label: 'Docker', id:'docker'},
+        {value: 10, label: 'Go', id:'go'},
+        {value: 5, label: 'Googling', id:'googling'},
+      ],
+      backgroundColor: '#fff',
+      labelColor: 'black',
+      colors: [
+        '#F89820',
+        '#0055A6',
+        '#E1E807',
+        '#0BA462',
+        '#0db7ed',
+        '#BA9F4E',
+        '#95D7BB'
+      ],
+      formatter: function (x,data) {
+          highlightSkill(data["id"]);
+
+          return x + "%";
+      }
+    });
+});
+
+function highlightSkill(skill){
+    console.log(" data: "+data["id"]);
+}
