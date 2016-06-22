1d86b77b16bb7b22d6136e35bb41e37367ec0e56
Horacio Delgado <horacio.delgado23@gmail.com>
2016-06-22 15:25:20 -0400

    making the feed dynamic

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,8 @@
-(function($) {
 
-// prettyPhoto
-	jQuery(document).ready(function(){
-		jQuery('a[data-gal]').each(function() {
-			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
-		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
 
-		
-})(jQuery);
+$(document).ready(function(){
+	$.get("https://github.com/users/horacio23/contributions", function(data){
+		console.log(data);
+		$("#activityImage").html(data);
+	});
+});
